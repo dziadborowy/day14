049fd9e3dfa96def274fb57fc0476b42d7d9d7b9
dziadborowy <adam.chudeusz@gmail.com>
2017-03-16 19:26:22 +0100

    parametry do cmd

--- kalk2.py
+++ kalk2.py
@@ -1,1 +1,18 @@
-aaaa
+import sys
+
+# print(sys.argv)
+
+def divide(a,b):
+    return a/b
+
+
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+
+    wynik = divide(a, b)
+    print(wynik)
+
+
+if __name__ == '__main__':
+    main()
